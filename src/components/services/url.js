89776f6b7bdb9afaa4da51e6b83ffe966b36f949
Phuong <phuongphuong262204@gmail.com>
2024-04-22 09:38:58 +0700

    update api

---
+++ src/components/services/url.js
@@ -0,0 +1,37 @@
+const url = {
+    BASE_URL: "https://localhost:7270/api",
+    MENU: {
+        LIST: "/menu",
+    },
+
+    MOVIE: {
+        LIST: "/movie",
+        DETAIL: "/movie/{}",
+        CREATE: "/movie/create",
+        UPDATE: "/movie/edit",
+        TRASH: "/movie/trash-can",
+        DELETE: "/movie/delete",
+        RESTORE: "/movie/restore/{}",
+    },
+
+    GALLERY: {
+        LIST: "/gallery/get-all",
+        DETAIL: "/gallery/get-by-id/{}",
+        CREATE: "/gallery/create",
+        UPDATE: "/gallery/edit",
+        TRASH: "/gallery/trash-can",
+        DELETE: "/gallery/delete",
+        RESTORE: "/gallery/restore/{}",
+    },
+
+    AUTH: {
+        LOGIN: "/auth/login",
+        FORGOT_PASSWORD: "/auth/forgot-password",
+        PROFILE: "/auth/profile",
+        UPDATE_PROFILE: "/auth/update-profile",
+        CHANGE_PASSWORD: "/auth/change-password",
+        RESET_PASSWORD: "auth/reset-password",
+    },
+
+};
+export default url;
