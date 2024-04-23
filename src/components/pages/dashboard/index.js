d22d0f8b4ae6819a0e573fbe2b70cd4bf7538a17
Phuong <phuongphuong262204@gmail.com>
2024-04-23 16:12:35 +0700

    Update index.js

--- src/components/pages/dashboard/index.js
+++ src/components/pages/dashboard/index.js
@@ -136,7 +136,7 @@ function Dashboard() {
                                                                     <svg width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-shopping-cart "><g><circle cx="9" cy="21" r="1"></circle><circle cx="20" cy="21" r="1"></circle><path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path></g></svg>
                                                                     </div>
                                                                     <div className=" ">
-                                                                        <h4 className="fs-18 font-w600 mb-1 text-break">Total Show</h4>
+                                                                        <h4 className="fs-18 font-w600 mb-1 text-break">Total Order</h4>
                                                                         <span className="fs-14">
                                                                             {/* {totalShowAndUp.totalShows} ({totalShowAndUp.upcomingShows} Upcoming) */}
                                                                         </span>
